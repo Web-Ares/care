e1000c766b9cf506b35e8227c018f90b222caa43
petruninevgeniy <petruninevgeniy@gmial.com>
2016-07-05 18:43:40 +0300

    index pre

--- app/js/jquery.index.js
+++ app/js/jquery.index.js
@@ -1,3 +1,43 @@
-( function(){
+$(function(){
 
-} )();
+    "use strict";
+
+    $( function(){
+
+        $.each( $( '.promo' ), function() {
+            new Sliders ( $( this ) );
+        } );
+
+    } );
+
+    var Sliders = function( obj ) {
+
+        //private properties
+        var _obj = obj,
+            _promoSlider = _obj.find( '.promo__swiper'),
+            _slider;
+
+
+        //private methods
+        var _initSlider = function() {
+
+                _slider = new Swiper ( _promoSlider, {
+                    autoplay: 4000,
+                    speed: 500,
+                    effect : 'fade',
+                    loop: true
+                } );
+
+            },
+            _init = function() {
+                _initSlider();
+            };
+
+        //public properties
+
+        //public methods
+
+        _init();
+    };
+
+} );
